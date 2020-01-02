0733e57ddf960c3b055279611eee4f2e9722fc5b
ProloySimulation <xtrmproloy875@gmail.com>
2020-01-02 18:24:02 +0600

    API Added

--- app/src/main/java/com/example/proloy/lifesaver/ActivityLogin.java
+++ app/src/main/java/com/example/proloy/lifesaver/ActivityLogin.java
@@ -6,12 +6,25 @@ import android.os.Bundle;
 import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ActivityLogin extends AppCompatActivity {
 
     Button btnLogin ;
     TextView tvSignUp ;
+    EditText etLoginEmail , etLoginPassword ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +33,48 @@ public class ActivityLogin extends AppCompatActivity {
 
         btnLogin=findViewById(R.id.btnLogin);
         tvSignUp = findViewById(R.id.tvSignUp);
+        etLoginEmail = findViewById(R.id.etLoginEmail);
+        etLoginPassword = findViewById(R.id.etLoginPassword);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                login();
                 Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                 startActivity(intent);
             }
         });
 
+    }
+
+    // Login Using Volley
+
+    private void login() {
+
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "blabal",
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        Toast.makeText(ActivityLogin.this,"Register Scucessfully",Toast.LENGTH_SHORT).show();
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String , String> params = new HashMap<>();
+                params.put("username",etLoginEmail.getText().toString());
+                params.put("password",etLoginPassword.getText().toString());
+                return params;
+            }
+        };
+
+        Volley.newRequestQueue(this).add(stringRequest);
     }
 }
--- app/src/main/java/com/example/proloy/lifesaver/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/proloy/lifesaver/Fragment/HomeFragment.java
@@ -13,13 +13,14 @@ import android.widget.Button;
 import com.example.proloy.lifesaver.ActivityApointment;
 import com.example.proloy.lifesaver.ActivityBlood;
 import com.example.proloy.lifesaver.ActivityCatagory;
+import com.example.proloy.lifesaver.QuestionActivity;
 import com.example.proloy.lifesaver.R;
 
 
 public class HomeFragment extends Fragment {
 
     Button btnDoctor ;
-    Button btnBlood ;
+    Button btnBlood,btnQuetion ;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -34,6 +35,7 @@ public class HomeFragment extends Fragment {
 
         btnDoctor = (Button) myFragmentView.findViewById(R.id.btn1);
         btnBlood =  (Button) myFragmentView.findViewById(R.id.btn2);
+        btnQuetion = myFragmentView.findViewById(R.id.btn4);
 
         btnDoctor.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,6 +49,14 @@ public class HomeFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getActivity().getApplicationContext(),ActivityBlood.class);
+                startActivity(intent);
+            }
+        });
+
+        btnQuetion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), QuestionActivity.class);
                 startActivity(intent);
             }
         });
